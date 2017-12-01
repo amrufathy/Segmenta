5ce8894eeb87983240d01d63030dad88a27a4046
Nader Alsharkawy <subnader@gmail.com>
2017-12-01 23:29:29 +0200

    Update data loader and add Python 3 support

--- lib/dataloader.py
+++ lib/dataloader.py
@@ -5,20 +5,16 @@ import os
 from PIL import Image
 from scipy.io import loadmat
 
-
-def load_data(data_directory='../data'):
+def load_training(data_directory='../data'):
+    
     # Define lists
     training_set = list()
     training_gt = list()
-    test_set = list()
-    test_gt = list()
-    validation_set = list()
-    validation_gt = list()
 
     # Load training set and its ground truth
     training_directory = data_directory + '/images/train'
     training_gt_directory = data_directory + '/groundTruth/train'
-    training_count = len(os.walk(training_directory).next()[2])
+    training_count = len(os.walk(training_directory).__next__()[2])
     training_images_names = os.listdir(training_directory)
     for image_name in training_images_names:
         image_gt_file_name = image_name.split('.')[0] + '.mat'
@@ -26,11 +22,23 @@ def load_data(data_directory='../data'):
         image_gt = loadmat(training_gt_directory + '/' + image_gt_file_name)
         training_set.append(image_file)
         training_gt.append(image_gt)
+    
+    # Print stats
+    print("Loaded training data succesfully.\n\nSet size = ", training_count, "samples.\n")
+
+    # Return sets
+    return training_set, training_gt
+
+def load_test(data_directory='../data'):
+    
+    # Define lists
+    test_set = list()
+    test_gt = list()
 
     # Load test set and its ground truth
     test_directory = data_directory + '/images/test'
     test_gt_directory = data_directory + '/groundTruth/test'
-    test_count = len(os.walk(test_directory).next()[2])
+    test_count = len(os.walk(test_directory).__next__()[2])
     test_images_names = os.listdir(test_directory)
     for image_name in test_images_names:
         image_gt_file_name = image_name.split('.')[0] + '.mat'
@@ -38,11 +46,23 @@ def load_data(data_directory='../data'):
         image_gt = loadmat(test_gt_directory + '/' + image_gt_file_name)
         test_set.append(image_file)
         test_gt.append(image_gt)
+    
+    # Print stats
+    print("Loaded test set succesfully.\n\nSet size = ", test_count, "samples.\n")
+
+    # Return sets
+    return test_data, test_gt
+
+def load_validation(data_directory='../data'):
+    
+    # Define lists
+    validation_set = list()
+    validation_gt = list()
 
     # Load validation set and its ground truth
     validation_directory = data_directory + '/images/val'
     validation_gt_directory = data_directory + '/groundTruth/val'
-    validation_count = len(os.walk(validation_directory).next()[2])
+    validation_count = len(os.walk(validation_directory).__next__()()[2])
     validation_images_names = os.listdir(validation_directory)
     for image_name in validation_images_names:
         image_gt_file_name = image_name.split('.')[0] + '.mat'
@@ -50,11 +70,13 @@ def load_data(data_directory='../data'):
         image_gt = loadmat(validation_gt_directory + '/' + image_gt_file_name)
         validation_set.append(image_file)
         validation_gt.append(image_gt)
-
+    
     # Print stats
-    print("Loaded the dataset succesfully.\n\nTraining set size = ", training_count, "samples.\nTest set size = ", \
-          test_count, "samples.\nValidation set size = ", validation_count, "samples.\n\nData set size = ", \
-          (training_count + test_count + validation_count), "samples.\n")
+    print("Loaded validation set succesfully.\n\nSet size = ", validation_count, "samples.\n")
 
     # Return sets
-    return training_set, training_gt, test_set, training_gt, validation_set, validation_gt
+    return validation_set, validation_gt
+
+if __name__ == '__main__':
+    val, val_gt = load_validation()
+    print(len(val), len(val_gt))
