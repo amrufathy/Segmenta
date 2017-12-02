1fbfdcb90dd0cad1e28b85ab62a2b817c752c887
Amr Fathy <amruafathy@gmail.com>
2017-12-03 00:01:28 +0200

    Add boundaries generation

--- lib/utils.py
+++ lib/utils.py
@@ -11,22 +11,29 @@ def show_image_segmentations(image):
     image_path = os.getcwd() + '/data/images/test/' + image + '.jpg'
     img = Image.open(image_path)
 
-    f, axarr = plt.subplots(2, 3)
+    f1, axarr1 = plt.subplots(2, 3)  # segmentations
+    f2, axarr2 = plt.subplots(2, 3)  # boundaries
 
     plt.suptitle('Visualization of image ' + image + ' with its segmentation')
 
-    axarr[0, 0].imshow(img)
-    axarr[0, 0].set_title('Image')
+    axarr1[0, 0].imshow(img)
+    axarr1[0, 0].set_title('Image')
+
+    axarr2[0, 0].imshow(img)
+    axarr2[0, 0].set_title('Image')
 
     i = 0
     j = 1
 
     for segmentation, boundary in load_segmentations(image):
         k = np.unique(np.ravel(segmentation)).size
-        axarr[i, j].imshow(segmentation)
-        axarr[i, j].set_title('Seg k = {}'.format(k))
+        axarr1[i, j].imshow(segmentation)
+        axarr1[i, j].set_title('Seg k = {}'.format(k))
+
+        axarr2[i, j].imshow(boundary, cmap='Greys')
 
         i = i + 1 if j == 2 else i
         j = 0 if j + 1 == 3 else j + 1
 
-    f.savefig('image_viz.png')
+    f1.savefig('image_seg_viz.png')
+    f2.savefig('image_bound_viz.png')
